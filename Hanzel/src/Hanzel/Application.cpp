81b00f2309c9ac0093ebc037cf250864497b1656
hantiantian2 <2675509882@qq.com>
2020-09-12 16:23:03 +0800

    Setup base Application and Entry Point

---
+++ Hanzel/src/Hanzel/Application.cpp
@@ -0,0 +1,30 @@
+#include "Application.h"
+#include <stdio.h>
+
+namespace Hazel 
+{
+	Application::Application()
+	{
+
+	}
+
+	Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		printf("Hello World!!!\n");
+		while (true)
+		{
+
+		}
+
+	}
+}
+
+
+
+
+
---
+++ Hanzel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//For use by Hazel applications
+
+#include "Hanzel/Application.h"
+
+//Entry Point
+#include "Hanzel/EntryPoint.h"
